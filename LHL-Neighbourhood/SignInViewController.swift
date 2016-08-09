d1858ef54d884af6c180d02b2aad6484d02aecec
Asuka Nakagawa <nina-jamie.elle14@live.jp>
2016-08-08 18:08:11 -0700

    initial commit

---
+++ LHL-Neighbourhood/SignInViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SignInViewController.swift
+//  LHL-Neighbourhood
+//
+//  Created by Asuka Nakagawa on 2016-08-08.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+
+class SignInViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "bar"
+//        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+//            print("Object has been saved.")
+//        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
